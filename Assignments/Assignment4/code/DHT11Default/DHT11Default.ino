6d8fd9c46786e3ea9f8fb50586d651236b312cc4
UchitShriyan <uchitshriyan1999@gmail.com>
2023-10-07 23:48:16 -0700

    server.js changed

--- Assignments/Assignment4/code/DHT11Default/DHT11Default.ino
+++ Assignments/Assignment4/code/DHT11Default/DHT11Default.ino
@@ -12,7 +12,7 @@ const char* ssid = "Hotspot";
 const char* password = "longisland";
 
 //Your Domain name with URL path or IP address with path
-const char* serverName = "http://54.236.11.191:8080/sendData";
+const char* serverName = "http://54.236.11.191:1234/sendData";
 
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
